0176bc7e9d671feed2835558ba13cb0ce1c75d61
wangchunpeng <wangchunpeng@baidu.com>
2017-08-22 19:02:58 +0800

    add upload

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
  */
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
 
 var Posts = require('./posts.js');
 var Comments = require('./comments.js');
@@ -13,6 +14,12 @@ router.get('/', function(req, res, next) {
     res.render('index', {
         title: 'API for 我说呢'
     });
+});
+
+var upload = multer({ dest: 'uploads/' });
+router.post('/upload', upload.single('imgFile'), function (req, res, next) {
+	console.log(upload.filename);
+	res.send('ok');
 });
 
 router = Posts(router);
